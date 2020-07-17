191ae730b3addb52adb601f32008a85348e01735
adityajm96 <adityajm96@gmail.com>
2020-07-17 10:31:18 +0530

    Updated config and server files

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
-//const Koa = require('koa');
-const express=require ('express')
-//const parser = require('koa-parser')
+const Koa = require('koa');
+//const express=require ('express')
+const parser = require('koa-parser')
 const bodyParser = require('body-parser')
 const port = 3000
-//const app = new Koa();
-const app = express();
+const app = new Koa();
+//const app = express();
 const router = require('./routes');
 const db = require('./models');
-// const serve = require('koa-static');
+const serve = require('koa-static');
 const path = require('path');
-//app.use(bodyparser());
+app.use(bodyparser());
 app.use(bodyParser.json())
 
 db.sequelize.sync().then(() => {
@@ -22,8 +22,8 @@ app
   .use(router.routes())
   .use(router.allowedMethods());
 
-  app.use(express.static(path.join(__dirname, 'public')));
-  // app.use(serve(__dirname + '/public'));
+  //app.use(express.static(path.join(__dirname, 'public')));
+   app.use(serve(__dirname + '/public'));
 
 app.listen(port)
 console.error(`listening on port ${port}`)
